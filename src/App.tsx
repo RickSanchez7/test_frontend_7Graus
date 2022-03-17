fe9ce7692d6173164c07b21a24a6b75d265a7aa4
RickSanchez7 <61505211+RickSanchez7@users.noreply.github.com>
2022-03-17 13:33:24 +0000

    fetch 10 users on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { Users } from './interfaces/users';
+import { useFetchUsers } from './useHooks/useFetchUsers';
 
 function App() {
+  const { users, loading, error, fetchUsers } = useFetchUsers();
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <div>
-      <h1>Hello world</h1>
+      {users.map((user: Users) => (
+        <div key={user.id}>
+          <h1>
+            {user.name.title} {user.name.first} {user.name.last}
+          </h1>
+          <img src={user.picture.thumbnail} alt={user.name.first} />
+        </div>
+      ))}
     </div>
   );
 }
